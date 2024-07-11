996908abce9758d0d3a9206c647554ab67e962e5
unknown <kanakalamanikanta2006@gmail.com>
2024-07-11 14:36:58 +0530

    second

--- src/components/Home/Carousel.jsx
+++ src/components/Home/Carousel.jsx
@@ -4,22 +4,17 @@ import class2 from '../../assets/class2.jpg'
 import class3 from '../../assets/class3.jpg'
 import class4 from '../../assets/class4.jpg'
 const Carousel = () => {
-  return (
-
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  return(<>
+      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
-    
-    <div class="carousel-item">
+    <div class="carousel-item active">
       <img src={class1} class="d-block w-100" alt="..."/>
     </div>
-    <div class="carousel-item active">
+    <div class="carousel-item">
       <img src={class2} class="d-block w-100" alt="..."/>
     </div>
     <div class="carousel-item">
       <img src={class3} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={class4} class="d-block w-100" alt="..."/>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -30,9 +25,9 @@ const Carousel = () => {
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
-  
 </div>
-    )
+    </>
+  )
 }
 
 export default Carousel
